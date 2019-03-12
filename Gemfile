8270e9f0ba7eb5eed8192a4788a0810ad12b6c4b
Bahar <pourazar.bahare@gmail.com>
2019-03-12 18:19:44 -0400

    rack unit tests

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'colorize', '~> 0.8.1'
 gem 'rack', '~>2.0.6'
+gem 'rack-test'
+gem 'minitest'
---
+++ test/app_test.rb
@@ -0,0 +1,33 @@
+require 'rack/test'
+require 'minitest/autorun'
+
+OUTER_APP = Rack::Builder.parse_file("#{__dir__}/../config.ru").first
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    OUTER_APP
+  end
+
+  def test_root
+    get '/'
+    assert last_response.ok?
+    assert_equal last_response.content_type, 'text/plain'
+    assert last_response.body == 'Hello World'
+  end
+
+  def test_params
+    get '/params?foo=1'
+    assert last_response.ok?
+    assert_equal last_response.content_type, 'application/json'
+    assert last_response.body == last_request.params.to_json
+  end
+
+  def test_not_found
+    get '/bar'
+    assert last_response.not_found?
+    assert last_response.body == 'Not found'
+  end
+
+end
